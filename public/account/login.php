75082a1ee478718de99ab2b764eede77796708f7
Stephen <harkugbeosaz@gmail.com>
2022-03-29 14:29:27 +0100

    Login page changed

--- public/account/login.php
+++ public/account/login.php
@@ -31,32 +31,6 @@ if(isset($_SESSION['user'])){
             <div class="col-lg-4 mx-auto rounded" id="login-box"  style="background: white">
                 <h2 class="text-center mt-2">Login</h2>
                 <h6 class="text-center my-2">Welcome back</h6>
-                <!-- <form action="" method="post" role="form" class="p-2" id="login-form">
-                    <div class="form-group">
-                    <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" class="form-control" placeholder="Enter email" required>
-                    </div>
-                    <div class="input-group">
-                    <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" class="form-control" placeholder="Enter password" required minlength="8">
-                        <span class="input-group-text">.00</span>
-                        <div class="input-group-append my-1" role="button">
-                            <small><input type="checkbox" id="showpass"> Show Password</small>
-                        </div>
-                    </div>
-                    <div class="form-group mt-3">
-                        <div class="custom-control custom-checkbox">
-                            <a href="forgotpass.php" id="forgot-btn" class="float-right text-secondary">Forgot Password?</a>
-                        </div>
-                    </div>
-                    div class="form-group">
-                        <input type="submit" name="login" id="login" value="Login" class="btn btn-primary btn-block">
-                    </div> 
-                    <div class="form-group">
-                        <p class="text-center">Don't have an account yet?<a href="register.php" id="register-btn" class="text-secondary"> Register here</a></p>
-                    </div> 
-                </form> -->
-
                 <form action="" method="post" role="form" class="p-2" id="login-form">
                     <div class="mb-3">
                         <label for="email" class="form-label">Email address</label>
@@ -86,9 +60,6 @@ if(isset($_SESSION['user'])){
     </div>
 </section>
 
-<?php
-// include_once "footer.php";
-?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
 
